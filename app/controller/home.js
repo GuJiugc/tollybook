c6bb4d687769d3b0cef3a40c3361416e4067eed9
guocong <875219399@qq.com>
2022-11-09 10:04:23 +0800

    '记账本后台完成'

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,10 +3,7 @@
 const { Controller } = require('egg');
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
+  
 }
 
 module.exports = HomeController;
--- config/plugin.js
+++ config/plugin.js
@@ -6,4 +6,20 @@ module.exports = {
   // static: {
   //   enable: true,
   // }
+  ejs: {
+    enable: true,
+    package: 'egg-view-ejs'
+  },
+  mysql: {
+    enable: true,
+    package: 'egg-mysql'
+  },
+  jwt: {
+    enable: true,
+    package: 'egg-jwt'
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
 };
